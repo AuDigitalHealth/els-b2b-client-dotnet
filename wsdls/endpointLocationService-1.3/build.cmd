f9e4a294324d7333b6568b7cffe2e1bccb027a06
philipwilford <philip.wilford@digitalhealth.gov.au>
2018-04-30 16:30:24 +1000

    updated path for compilation - does not affect project

--- wsdls/endpointLocationService-1.3/build.cmd
+++ wsdls/endpointLocationService-1.3/build.cmd
@@ -1,5 +1,5 @@
 cls
-SET WSDLTOOL="c:\Program Files\Microsoft SDKs\Windows\v6.0A\Bin\SvcUtil.exe"
+SET WSDLTOOL="c:\Program Files (x86)\Microsoft SDKs\Windows\v7.0A\Bin\SvcUtil.exe"
 
 %WSDLTOOL% wsdl\*.wsdl xsd\*.xsd /out:ELSv2010.cs /noConfig /namespace:*,Nehta.ELSv2010
 
